18d6d002bb27153fff673f37343b8795ab866c46
Nguyệt <trcngt@MacBook-Pro-cua-Nguyet.local>
2026-08-13 16:50:34 +0700

    style: chi hien thi danh muc cua tang hien tai tren sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
 		starlight({
 			title: 'Footypedia',
 			description: 'Bách khoa toàn thư bóng đá bài bản',
+			customCss: ['./src/styles/custom.css'], // <-- Thêm dòng này vào
 			sidebar: [
 				{
 					label: '1. Luật Bóng Đá',
